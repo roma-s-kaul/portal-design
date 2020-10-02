c1457d2c1b3a4f220016d06b7b4b000c519e1eb8
Roma Kaul <roma.s.kaul@gmail.com>
2020-10-01 17:32:37 -0700

    Updates

---
+++ precache-manifest.954d45095016769ba3890c9193d3b5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316b1549db6d263dc2a9e92772c87bbd",
+    "url": "/portal-design/index.html"
+  },
+  {
+    "revision": "f88d7debccb4576cf9cf",
+    "url": "/portal-design/static/css/main.35f8106a.chunk.css"
+  },
+  {
+    "revision": "c94f4c241b2995febcc0",
+    "url": "/portal-design/static/js/2.e9f32ce6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portal-design/static/js/2.e9f32ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88d7debccb4576cf9cf",
+    "url": "/portal-design/static/js/main.6d7315b3.chunk.js"
+  },
+  {
+    "revision": "4bdce25182783fee57aa",
+    "url": "/portal-design/static/js/runtime-main.94fb1318.js"
+  }
+]);
